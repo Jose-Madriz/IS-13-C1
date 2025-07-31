584769dfff6d690adde165906671690361e68de7
Fred Urbina <Fredrbina@gmail.com>
2025-07-31 03:06:25 -0400

    Añadiendo  varios metodos para carga y actualizacion de base de datos,

--- src/main/Models/ModeloMenuPrincipal/Menu.java
+++ src/main/Models/ModeloMenuPrincipal/Menu.java
@@ -1,8 +1,12 @@
 package main.Models.ModeloMenuPrincipal;
 
-import java.io.BufferedReader;
+import java.io.*;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Menu {
     public String turno;
@@ -10,6 +14,7 @@ public class Menu {
     public String horario;
     public double calorias;
     public double precio;
+    private String dbPath;
 
     public Menu(){
         
@@ -18,14 +23,18 @@ public class Menu {
         this.horario = "";
         this.calorias = 0;
         this.precio = 0;
+        this.dbPath ="DataBaseMenu.txt";
+
+        this.fetchMenus();
     }
-
     public Menu( String _turno,String _platillo, String _horario, double _calorias, double _precio ){
-
+        
         this.turno = _turno;
         this.platillo = _platillo;
-        this.horario = _horario;
-        this.calorias = _calorias;
+        this.horario = _horario; // Assign _horario to this.horario
+        this.calorias = _calorias; // Assign _calorias to this.calorias
+        this.precio = _precio; // Assign _precio to this.precio
+        this.dbPath = "DataBaseMenu.txt"; // Initialize dbPath
     }
 
     public Menu menu1 = null;
@@ -33,33 +42,30 @@ public class Menu {
 
     public void fetchMenus(){
         int i = 0;
-        try{
-            BufferedReader lector = new BufferedReader(new FileReader("DataBaseMenu.txt"));
+
+        try {
+            BufferedReader lector = new BufferedReader(new FileReader(this.dbPath));
             String linea="";
             while ((linea = lector.readLine()) != null) {
                 String [] bloques = linea.split(",");
-                if( bloques.length == 4){
+                if( bloques.length == 5){
                     if(i==0){
                         String turno = bloques[0];
                         String platillo = bloques[1];
                         String horario =  bloques[2];
                         double calorias = Double.parseDouble(bloques[3]);
+                        Double precio = Double.parseDouble(bloques[4]);
 
-                        menu1.turno = turno;
-                        menu1.platillo = platillo;
-                        menu1.horario = horario;
-                        menu1.calorias = calorias;
+                        this.menu1 = new Menu(turno, platillo, horario, calorias, precio);
                         i++;
                     }else if(i == 1){
                         String turno = bloques[0];
                         String platillo = bloques[1];
                         String horario = bloques[2];
                         double calorias = Double.parseDouble(bloques[3]);
-
-                        menu2.turno = turno;
-                        menu2.platillo = platillo;
-                        menu2.horario = horario;
-                        menu2.calorias = calorias; 
+                        Double precio = Double.parseDouble(bloques[4]);
+                        
+                        this.menu2 = new Menu(turno, platillo, horario, calorias, precio);
                         i++; 
                     }
                 }
@@ -77,6 +83,24 @@ public class Menu {
         return (menu2.turno != "" && menu2.platillo != "" && menu2.horario != "" && menu2.calorias != 0);
     }
     public void rewriteMenus() {
-        // TODO implementar
+        String filePath = this.dbPath + ".tmp";
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(filePath))) {
+
+            if (menu1Exist()) {
+                String line = menu1.turno + "," + menu1.platillo + "," + menu1.horario + "," + menu1.calorias + "," + menu1.precio;
+                writer.write(line);
+                writer.newLine();
+            }
+            if (menu2Exist()) {
+                String line = menu2.turno + "," + menu2.platillo + "," + menu2.horario + "," + menu2.calorias + "," + menu2.precio;
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.flush();
+            Files.deleteIfExists(Paths.get("DataBaseMenu.txt"));
+            Files.move(Paths.get(filePath), Paths.get("DataBaseMenu.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
